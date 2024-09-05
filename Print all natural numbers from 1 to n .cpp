a5bf3a3a156e78974fca7e8aed99b399c0520408
Vishal kushwah <110225964+vishalkushw@users.noreply.github.com>
2024-09-05 22:07:34 +0530

    Print all natural numbers from 1 to n

---
+++ Print all natural numbers from 1 to n .cpp
@@ -0,0 +1,16 @@
+ // Write a program to print all natural numbers from 1 to n
+
+#include <iostream>
+using namespace std;
+int main(){
+	
+	int n;
+	cout<<"Enter your number : " ;
+	cin>>n;
+	
+	for(int i=1;i<=n; i++){
+		cout<<i<<endl; 
+	} 
+}
+
+

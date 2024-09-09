b297298fa5c40fffb09d4cef437e856ae84efaaa
Vishal kushwah <110225964+vishalkushw@users.noreply.github.com>
2024-09-10 00:29:32 +0530

    Convert lowercase to uppercase

---
+++ Convert lowercase to uppercase.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string str;
+    cout << "Enter a string: ";
+    getline(cin, str);
+
+    for (int i = 0; i < str.length(); i++) {
+        if (str[i] >= 'a' && str[i] <= 'z') {
+            str[i] = str[i] - 32;  
+        }
+    }
+
+    cout << "Uppercase string: " << str << endl;
+}
